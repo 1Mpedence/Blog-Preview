331a4e52af70d565227247eef0cd6d48a54593cc
1Mpedence <harshanand27@gmail.com>
2024-02-07 19:55:34 +0530

    class -> className

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 export default function Footer(){
     return (
         <footer>
-            <div class="attribution">
+            <div className="attribution">
                 Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.
                 Coded by <a href="https://www.frontendmentor.io/profile/1Mpedence" target="_blank" rel="noreferrer">Harsh Anand</a>.
             </div>
